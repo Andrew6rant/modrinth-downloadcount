6b543e67a00ef69eedcbefeb3cf1a44830bdcc9a
Andrew Grant <57331134+Andrew6rant@users.noreply.github.com>
2021-08-17 19:29:41 -0400

    Update get_downloads.py

--- get_downloads.py
+++ get_downloads.py
@@ -7,9 +7,8 @@ def get_downloads(id):
     for mod_id in number_of_mods:
         b = requests.get('https://api.modrinth.com/api/v1/mod/'+mod_id)
         e = b.json()
-        print(int(e["downloads"]))
-    else:
-        print("done")
+        total_downloads += e["downloads"]
+    print(total_downloads)
 
 if __name__ == '__main__':
     get_downloads('IV9h2DkJ')
