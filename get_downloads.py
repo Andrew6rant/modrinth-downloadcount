da88820f2382c07f10f500c7ca9dc4473a2f1e4d
Andrew Grant <57331134+Andrew6rant@users.noreply.github.com>
2021-08-17 19:31:17 -0400

    slightly better naming

--- get_downloads.py
+++ get_downloads.py
@@ -1,13 +1,13 @@
 import requests
 
-def get_downloads(id):
-    r = requests.get('https://api.modrinth.com/api/v1/user/'+id+'/mods')
+def get_downloads(user_id):
+    r = requests.get('https://api.modrinth.com/api/v1/user/'+user_id+'/mods')
     number_of_mods = r.json()
     total_downloads = 0
     for mod_id in number_of_mods:
         b = requests.get('https://api.modrinth.com/api/v1/mod/'+mod_id)
-        e = b.json()
-        total_downloads += e["downloads"]
+        mod_json = b.json()
+        total_downloads += mod_json["downloads"]
     print(total_downloads)
 
 if __name__ == '__main__':
